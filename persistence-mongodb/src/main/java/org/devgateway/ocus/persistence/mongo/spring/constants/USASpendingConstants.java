669565c321e55533e68317fc6de3aedae5940de8
Ionut Dobre <idobre@developmentgateway.org>
2016-08-05 12:21:33 -0400

    OCUS-10 - added mapping between NAICS codes and their description.

--- persistence-mongodb/src/main/java/org/devgateway/ocus/persistence/mongo/spring/constants/USASpendingConstants.java
+++ persistence-mongodb/src/main/java/org/devgateway/ocus/persistence/mongo/spring/constants/USASpendingConstants.java
@@ -1,13 +1,24 @@
 package org.devgateway.ocus.persistence.mongo.spring.constants;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.log4j.Logger;
 import org.devgateway.ocds.persistence.mongo.Tender;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author idobre
  * @since 7/22/16
  */
 public final class USASpendingConstants {
+    private static final Logger LOGGER = Logger.getLogger(USASpendingConstants.class);
+
+
+    public static Map<String, String> naicsCodes;
+
     public static final class AwardStatus {
         public static final String ACTIVE = "Active";
     }
@@ -21,5 +32,27 @@ public final class USASpendingConstants {
                         .put("D: FULL AND OPEN COMPETITION AFTER EXCLUSION OF SOURCES",
                                 Tender.ProcurementMethod.selective)
                         .build();
+    }
+
+    public static String getNaicsCodeDescription(String code) {
+        // read the codes from the properties file
+        if (naicsCodes == null) {
+            naicsCodes = new HashMap<>();
+
+            Properties props = new Properties();
+            try {
+                props.load(USASpendingConstants.class.getResourceAsStream("naics-6-digit-codes.properties"));
+            } catch (IOException e) {
+                LOGGER.error(e);
+            }
+
+            for (Object obj : props.keySet()) {
+                String key = obj.toString();
+                String description = props.getProperty(key);
+                naicsCodes.put(key, description);
+            }
+        }
+
+        return naicsCodes.get(code);
     }
 }
